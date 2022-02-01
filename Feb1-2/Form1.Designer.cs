61df0b4fbfa0f03c687ecda8f85a816b97b51989
beatrisebertule <97166445+beatrisebertule@users.noreply.github.com>
2022-02-01 18:00:58 +0200

    large capitals

--- Feb1-2/Form1.Designer.cs
+++ Feb1-2/Form1.Designer.cs
@@ -44,7 +44,7 @@ namespace Feb1_2
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(222, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button for Courage";
+            this.button1.Text = "BUTTON for Courage";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -54,7 +54,7 @@ namespace Feb1_2
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(222, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button for Scoob";
+            this.button2.Text = "BUTTON for Scoob";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
